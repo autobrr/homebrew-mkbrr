996d922284053bfa3a01168d3ec714cce865818f
s0up4200 <18177310+s0up4200@users.noreply.github.com>
2025-06-05 20:17:35 +0000

    mkbrr v1.12.1

--- Formula/mkbrr.rb
+++ Formula/mkbrr.rb
@@ -4,8 +4,8 @@
 class Mkbrr < Formula
     desc "Command-line tool for creating, modifying and inspecting torrent files"
     homepage "https://github.com/autobrr/mkbrr"
-    url "https://github.com/autobrr/mkbrr/archive/refs/tags/v1.12.0.tar.gz"
-    sha256 "30d99ece976327416e855be00139b30a3e49aca67ae8df91193d88735f8dbbee"
+    url "https://github.com/autobrr/mkbrr/archive/refs/tags/v1.12.1.tar.gz"
+    sha256 "df4e7126d539181ce9d152b010baa0026d3be6147a77edc83b18acdba56ce566"
     license "GPL-2.0-or-later"
   
     depends_on "go" => :build
